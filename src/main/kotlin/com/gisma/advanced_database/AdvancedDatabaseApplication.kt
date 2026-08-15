0b7d99944cb296f46ad5a0909e834c1204f92219
nayara <nayara.dias@stations.cloud>
2026-08-15 23:58:05 +0200

    first commit

---
+++ src/main/kotlin/com/gisma/advanced_database/AdvancedDatabaseApplication.kt
@@ -0,0 +1,11 @@
+package com.gisma.advanced_database
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdvancedDatabaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdvancedDatabaseApplication>(*args)
+}
---
+++ src/test/kotlin/com/gisma/advanced_database/AdvancedDatabaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gisma.advanced_database
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdvancedDatabaseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
